ee76815381980760899bcad0975dc8507a35f012
Nathan <nathan@gdquest.com>
2017-12-11 18:58:30 +0100

    Add .import files
    Former-commit-id: e5b1503e55f59954472602ca9e86b9dd1c136c8f

--- godot-3-presentation/TouchControls.gd
+++ godot-3-presentation/TouchControls.gd
@@ -11,10 +11,12 @@ func _ready():
 
 
 func _on_mouse_entered():
+	print('enter')
 	$Tween.interpolate_property(self, 'modulate', modulate, COLOR_OPAQUE, TRANSITION_DURATION, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
 
 
 func _on_mouse_exited():
+	print('exit')
 	$Tween.interpolate_property(self, 'modulate', modulate, COLOR_TRANSPARENT, TRANSITION_DURATION, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
