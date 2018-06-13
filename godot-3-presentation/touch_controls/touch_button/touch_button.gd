0e71ad4eb01dc0d9ec8dbcf519df298f76962b75
Nathan <nathan@gdquest.com>
2018-06-13 11:31:39 +0900

    Add touch controls
    closes #12

--- godot-3-presentation/touch_controls/touch_button/touch_button.gd
+++ godot-3-presentation/touch_controls/touch_button/touch_button.gd
@@ -1,8 +1,9 @@
-extends TextureRect
+extends Button
 
+signal touched(button)
 
-const COLOR_TRANSPARENT = Color(1.0, 1.0, 1.0, 0.0)
-const COLOR_OPAQUE = Color(1.0, 1.0, 1.0, 1.0)
+const COLOR_TRANSPARENT = Color("#00ffffff")
+const COLOR_OPAQUE = Color("#ffffffff")
 
 const TRANSITION_DURATION = 0.4
 
@@ -11,12 +12,14 @@ func _ready():
 
 
 func _on_mouse_entered():
-	print('enter')
 	$Tween.interpolate_property(self, 'modulate', modulate, COLOR_OPAQUE, TRANSITION_DURATION, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
 
 
 func _on_mouse_exited():
-	print('exit')
 	$Tween.interpolate_property(self, 'modulate', modulate, COLOR_TRANSPARENT, TRANSITION_DURATION, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	$Tween.start()
+
+
+func _on_pressed():
+	emit_signal("touched", self)
