6e7b69f2289ca2f27b4e2841b661600468fe5a67
Nathan Lovato <nathan@gdquest.com>
2018-07-20 14:01:32 +0900

    Fix wrong variable name
    Former-commit-id: 7cc5affb76bd2a8b03d8d60bf79cb332e326fd2a

--- godot-3-presentation/slide/Slide.gd
+++ godot-3-presentation/slide/Slide.gd
@@ -14,6 +14,6 @@ func _on_Presentation_new_slide_requested(slide_data):
 		'background': '',
 		'config': {}
 	}
-	for key in data.keys():
-		content[key] = data[key]
+	for key in slide_data.keys():
+		content[key] = slide_data[key]
 	emit_signal("content_updated", content)
