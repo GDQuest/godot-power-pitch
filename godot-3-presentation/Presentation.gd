9c56605b850f6c494f5976d6d0b850e8e968ab80
Nathan Lovato <nathan@gdquest.com>
2018-07-20 13:57:40 +0900

    Reorder and simplify some code
    Former-commit-id: e81f98ae5cc9338f88cdc0c444f635d24306e9a0

--- godot-3-presentation/Presentation.gd
+++ godot-3-presentation/Presentation.gd
@@ -1,31 +1,26 @@
 extends Node
 
-enum DIRECTIONS {PREVIOUS = -1, CURRENT = 0, NEXT = 1}
-
 signal new_slide_requested(data)
 
-export(bool) var ADD_TEXT_SHADOW = true
-export(String, FILE, '*.gd') var slides_path = 'res://content/en.gd'
+enum DIRECTIONS {PREVIOUS = -1, CURRENT = 0, NEXT = 1}
+export(String, FILE, '*.gd') var SLIDES_PATH = 'res://content/en.gd'
 
 var slide_index = 0
 var slides_data = []
 
 func _ready():
-	slides_data = load(slides_path).new().data
-	display_slide(CURRENT)
+	slides_data = load(SLIDES_PATH).new().data
+	request_slide(CURRENT)
 	
-	if ADD_TEXT_SHADOW:
-		$Slide.shadow = true
-
 func _input(event):
 	if event.is_action_pressed('ui_next'):
-		display_slide(NEXT)
+		request_slide(NEXT)
 	if event.is_action_pressed('ui_previous'):
-		display_slide(PREVIOUS)
-
-func display_slide(direction=CURRENT):
-	slide_index = clamp(slide_index + direction, 0, len(slides_data) - 1)
-	emit_signal('new_slide_requested', slides_data[slide_index])
+		request_slide(PREVIOUS)
 
 func _on_TouchControls_slide_change_requested(direction):
-	display_slide(direction)
+	request_slide(direction)
+
+func request_slide(direction=CURRENT):
+	slide_index = clamp(slide_index + direction, 0, len(slides_data) - 1)
+	emit_signal('new_slide_requested', slides_data[slide_index])
