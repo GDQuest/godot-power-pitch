89cb06d1423dd2f9a1393bbb6b2ba11a7b1a6acc
Nathan Lovato <nathan@gdquest.com>
2018-09-12 15:07:21 +0900

    Add slides navigation to new presentation
    Add 3 temporary slides for testing purposes

--- godot-3-presentation/presentation/Slides.gd
+++ godot-3-presentation/presentation/Slides.gd
@@ -1,10 +1,21 @@
+tool
 extends Node
 
 enum DIRECTIONS {PREVIOUS = -1, CURRENT = 0, NEXT = 1}
 
 onready var slide_current = get_child(0)
 
+func _ready():
+	for slide in get_children():
+		slide.visible = false
+	slide_current.visible = true
+
 # TODO: remove slides from tree, store in array, and add/remove as needed?
 func display(direction=CURRENT):
-	var slide_index = clamp(slide_current.get_index() + direction, 0, len(get_children()) - 1)
-	slide_current = get_child(slide_index)
+	var previous_slide = slide_current
+	previous_slide.visible = false
+	
+	var new_slide_index = clamp(slide_current.get_index() + direction, 0, len(get_children()) - 1)
+	var new_slide = get_child(new_slide_index)
+	new_slide.visible = true
+	slide_current = new_slide
