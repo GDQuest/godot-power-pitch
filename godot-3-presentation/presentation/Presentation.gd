dea22c6846666cd0edacd05e289d2c7549e52b67
Nathan Lovato <nathan@gdquest.com>
2018-09-18 14:31:10 +0900

    Add basic swipe support to the presentation

--- godot-3-presentation/presentation/Presentation.gd
+++ godot-3-presentation/presentation/Presentation.gd
@@ -11,5 +11,11 @@ func _input(event):
 	if event.is_action_pressed('ui_previous'):
 		slides.display(slides.PREVIOUS)
 
+func _on_SwipeDetector_swiped(direction):
+	if direction.x == 1:
+		slides.display(slides.NEXT)
+	if direction.x == -1:
+		slides.display(slides.PREVIOUS)
+
 func _on_TouchControls_slide_change_requested(direction):
 	slides.display(direction)
--- godot-3-presentation/presentation/Slides.gd
+++ godot-3-presentation/presentation/Slides.gd
@@ -3,19 +3,24 @@ extends Node
 
 enum DIRECTIONS {PREVIOUS = -1, CURRENT = 0, NEXT = 1}
 
-var slide_current
+onready var slide_current = get_child(0)
 
 func initialize():
 	for slide in get_children():
 		slide.hide()
 	slide_current = get_child(0)
-	display(CURRENT)
+	slide_current.show()
 
 func display(direction=CURRENT):
 	var previous_slide = slide_current
-	previous_slide.hide()
 	
-	var new_slide_index = clamp(slide_current.get_index() + direction, 0, len(get_children()) - 1)
-	var new_slide = get_child(new_slide_index)
+	var previous_index = slide_current.get_index()
+	var new_index = clamp(slide_current.get_index() + direction, 0, len(get_children()) - 1)
+	if new_index == previous_index:
+		return
+
+	var new_slide = get_child(new_index)
 	new_slide.show()
+#	yield(new_slide.play("fade_in"), "completed")
+	previous_slide.hide()
 	slide_current = new_slide
