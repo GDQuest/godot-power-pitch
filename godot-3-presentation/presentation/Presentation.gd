d66278933e23e324f177a768e49f6fcddc0cf1d3
Nathan Lovato <nathan@gdquest.com>
2018-09-26 20:02:16 +0900

    Generate a csv file with unique ids for translations, closes #35
    Former-commit-id: ddef682c673f538144b16c0230381c627ed5eaaf

--- godot-3-presentation/presentation/Presentation.gd
+++ godot-3-presentation/presentation/Presentation.gd
@@ -4,6 +4,7 @@ onready var slides = $Slides
 
 func _ready():
 	slides.initialize()
+#	save_as_csv(get_translatable_strings()) # Use this to save the presentation as CSV
 
 func _input(event):
 	if event.is_action_pressed('ui_next'):
@@ -19,3 +20,57 @@ func _on_SwipeDetector_swiped(direction):
 
 func _on_TouchControls_slide_change_requested(direction):
 	slides.display(direction)
+
+func get_translatable_strings():
+	"""
+	Returns a dictionary with a list of { translatable_string_uid: string }
+	and the version of the project in which the data was generated
+	"""
+	var data = []
+	for node in get_tree().get_nodes_in_group("translate"):
+		var src_data = node.get_translation_data()
+		var node_uid = get_translation_uid(node)
+		for key in src_data:
+			var string_uid = node_uid + "_" + key
+			data.append({ string_uid: src_data[key] })
+	return {
+			'data': data,
+			'version': ProjectSettings.get_setting("application/config/version"),
+		}
+
+func save_as_csv(translation_data):
+	"""
+	Saves translation data from get_translatable_strings() to
+	this scene's folder, as scene_name.csv
+	"""
+	var folder_path = filename.left(filename.rfind("/") + 1)
+	var save_path = folder_path + name + ".csv"
+	var file = File.new()
+	
+	file.open(save_path, File.WRITE)
+	if not file.is_open():
+		print("Error saving translation data: could not open file %s" % save_path)
+		return
+
+	file.store_line("id,en")
+	var data_list = translation_data['data']
+	var csv_list = []
+	for dict in data_list:
+		for key in dict:
+			var as_csv = key + "," +  "\"" + dict[key] + "\""
+			csv_list.append(as_csv)
+	for line in csv_list:
+		file.store_line(line)
+	file.close()
+
+func translate_slides(locale):
+	TranslationServer.set_locale(locale)
+	for node in get_tree().get_nodes_in_group("translate"):
+		var node_uid = get_translation_uid(node)
+		var translatable_properties = node.get_translation_data()
+		for key in translatable_properties:
+			var string_uid = node_uid + "_" + key
+			node.set(key, tr(string_uid))
+
+func get_translation_uid(node):
+	return node.owner.name + "_" + str(node.owner.get_path_to(node)).replace("/", "_")
