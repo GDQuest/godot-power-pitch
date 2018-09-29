8f44287513598cee09c8f17bb654d891c5bdbee4
Nathan Lovato <nathan@gdquest.com>
2018-09-30 08:35:28 +0900

    Move translation code to Slides node, add  swipe animation,closes #11

--- godot-3-presentation/presentation/Slides.gd
+++ godot-3-presentation/presentation/Slides.gd
@@ -6,6 +6,12 @@ enum DIRECTIONS {PREVIOUS = -1, CURRENT = 0, NEXT = 1}
 var slide_current
 var slide_nodes = []
 
+func _input(event):
+	if event.is_action_pressed('ui_next'):
+		display(NEXT)
+	if event.is_action_pressed('ui_previous'):
+		display(PREVIOUS)
+
 func initialize():
 	for slide in get_children():
 		slide.hide()
@@ -16,6 +22,7 @@ func initialize():
 	slide_current.show()
 
 func display(direction=CURRENT):
+	set_process_input(false)
 	var previous_slide = slide_current
 	
 	var previous_index = slide_nodes.find(slide_current)
@@ -26,7 +33,27 @@ func display(direction=CURRENT):
 	var new_slide = slide_nodes[new_index]
 	add_child(new_slide)
 	new_slide.show()
-#	yield(new_slide.play("fade_in"), "completed")
+	
+#	var animation = "enter_from_right" if direction == NEXT else "enter_from_left"
+	var animation = "fade_in"
+	yield(new_slide.play(animation), "completed")
+	
 	previous_slide.hide()
 	remove_child(previous_slide)
 	slide_current = new_slide
+	
+	update_translations()
+	set_process_input(true)
+
+func update_translations():
+	for node in get_tree().get_nodes_in_group("translate"):
+		var node_uid = get_translation_uid(node)
+		var translatable_properties = node.get_translation_data()
+		for key in translatable_properties:
+			var string_uid = node_uid + "_" + key
+			node.set(key, tr(string_uid))
+		if node.has_method('translate'):
+			node.translate()
+
+func get_translation_uid(node):
+	return node.owner.name + "_" + str(node.owner.get_path_to(node)).replace("/", "_")
